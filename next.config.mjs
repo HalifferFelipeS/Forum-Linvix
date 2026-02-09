06c91a09f570a3772b3fb576f91a6de9ec1ca579
HalifferFelipeS <halifferfromao@gmail.com>
2026-02-09 12:57:08 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permite imagens de qualquer lugar (útil para o futuro)
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Permite que o Discord mostre seu site
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors * self https://*.discord.com https://discord.com;", // Libera o Discord
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
